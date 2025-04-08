1617fc1676d486f5b48f996bde7571c3ec0d235a
Helleson Allan <113473921+hellesonallan@users.noreply.github.com>
2025-04-08 13:25:50 +0000

    correçao feita no exercicio tabela frequencia

--- 03tabelafrequencia/TabelaFrequencia.java
+++ 03tabelafrequencia/TabelaFrequencia.java
@@ -4,22 +4,23 @@ public class TabelaFrequencia {
     public static Random gerador = new Random();
     
     public static void main(String[] args) {
-        int tamanho = 200;
-        int[] numeros = new int[tamanho];
-        int[] frequencia = new int[tamanho + 1];
+        final int tamanhoVetor = 200;
+        final int tamanhoGerador = 20;
+        int[] numeros = new int[tamanhoVetor];
+        int[] frequencia = new int[tamanhoVetor + 1];
 
-        for (int i = 0; i < tamanho; i++) {
-            numeros[i] = gerador.nextInt(tamanho) + 1;
+        for (int i = 0; i < tamanhoVetor; i++) {
+            numeros[i] = gerador.nextInt(tamanhoGerador) + 1;
         }
 
-        for (int i = 0; i < tamanho; i++) {
+        for (int i = 0; i < tamanhoVetor; i++) {
             int numero = numeros[i];
             frequencia[numero]++;
         }
 
         System.out.println("Número | Frequência");
         System.out.println("-------------------");
-        for (int i = 1; i <= tamanho; i++) {
+        for (int i = 1; i <= tamanhoVetor; i++) {
             if (frequencia[i] > 0) {
                 System.out.println(i + "      | " + frequencia[i]);
             }
